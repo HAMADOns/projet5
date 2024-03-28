5a653e54aead691bb54420eee545e04ef8f01432
Ons HAMAD <onshamadkl@gmail.com>
2024-03-28 02:49:01 +0100

    flex

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,27 @@
 import * as React from "react";
 import "./App.css";
+import "./style.css";
 
 function App() {
-  return <></>;
+  return (
+    <div className="wrapper">
+      <header className="header">
+        <h1>Header</h1>
+      </header>
+      <article className="main">
+        <p>kisdfhsd,fjgosdiufsdn,fbskdfbsd,nfkdusfgbjdsbf</p>
+      </article>
+      <aside className="aside aside1">
+        <h1>Aside 1</h1>
+      </aside>
+      <aside className="aside aside2">
+        <h1>Aside 2</h1>
+      </aside>
+      <footer className="footer">
+        <h1>Footer</h1>
+      </footer>
+    </div>
+  );
 }
 
 export default App;
